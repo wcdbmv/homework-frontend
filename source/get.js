7ef294c6b221a33c320af2829cc778dfb5c2c2f9
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-09-01 21:50:59 +0300

    Add jsDoc to get function

--- source/get.js
+++ source/get.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Gets the value at `path` of `object`.
+ * @param {Object} object
+ * @param {string} path
+ * @returns {*} value if exists else undefined
+ */
 const get = (object, path) => {
 	const keys = path.split('.').filter(key => key.length);
 	return keys.reduce(
